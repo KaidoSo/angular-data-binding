6d8e2dab8d6d127b4d49420829040653f6e62b29
Soomlais <kaido.soomlais@gmail.com>
2023-01-18 10:07:05 +0200

    add components and emit event

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,18 @@
+import {Component, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent {
+  intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber=0;
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000)
+  }
+}
